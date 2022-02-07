4b8a8ea729ea31bd7ca5ef45f10d45d96c06d38b
tobiasz-p <tpa0528@gmail.com>
2022-02-07 11:24:32 +0100

    Add scaffold assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :assignments
   devise_scope :user do
     # Redirests signing out users back to sign-in
     get 'users', to: 'devise/sessions#new'
