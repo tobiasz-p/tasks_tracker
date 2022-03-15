7ea0bf43ff87e5cc7599a0d5a88e78b6b3f44ab8
Tobiasz <57010376+tobiasz-p@users.noreply.github.com>
2022-03-15 14:34:47 +0100

    Update rails (#6)

--- db/schema.rb
+++ db/schema.rb
@@ -12,15 +12,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[6.1].define(version: 20_220_207_124_524) do
+ActiveRecord::Schema[7.0].define(version: 20_220_315_133_223) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
+
+  create_table 'active_storage_attachments', force: :cascade do |t|
+    t.string 'name', null: false
+    t.string 'record_type', null: false
+    t.bigint 'record_id', null: false
+    t.bigint 'blob_id', null: false
+    t.datetime 'created_at', null: false
+    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
+    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness',
+                                                    unique: true
+  end
+
+  create_table 'active_storage_blobs', force: :cascade do |t|
+    t.string 'key', null: false
+    t.string 'filename', null: false
+    t.string 'content_type'
+    t.text 'metadata'
+    t.string 'service_name', null: false
+    t.bigint 'byte_size', null: false
+    t.string 'checksum'
+    t.datetime 'created_at', null: false
+    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
+  end
+
+  create_table 'active_storage_variant_records', force: :cascade do |t|
+    t.bigint 'blob_id', null: false
+    t.string 'variation_digest', null: false
+    t.index %w[blob_id variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
+  end
 
   create_table 'assignments', force: :cascade do |t|
     t.bigint 'user_id', null: false
     t.bigint 'task_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
     t.boolean 'done'
     t.index %w[task_id user_id], name: 'index_assignments_on_task_id_and_user_id', unique: true
     t.index ['task_id'], name: 'index_assignments_on_task_id'
@@ -29,11 +58,11 @@ ActiveRecord::Schema[6.1].define(version: 20_220_207_124_524) do
 
   create_table 'tasks', force: :cascade do |t|
     t.bigint 'user_id', null: false
-    t.datetime 'due_date', precision: 6
+    t.datetime 'due_date'
     t.string 'title'
     t.text 'description'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
     t.integer 'done_assignments_count', default: 0, null: false
     t.index ['user_id'], name: 'index_tasks_on_user_id'
   end
@@ -42,19 +71,21 @@ ActiveRecord::Schema[6.1].define(version: 20_220_207_124_524) do
     t.string 'email', default: '', null: false
     t.string 'encrypted_password', default: '', null: false
     t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at', precision: 6
-    t.datetime 'remember_created_at', precision: 6
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
     t.string 'confirmation_token'
-    t.datetime 'confirmed_at', precision: 6
-    t.datetime 'confirmation_sent_at', precision: 6
+    t.datetime 'confirmed_at'
+    t.datetime 'confirmation_sent_at'
     t.string 'unconfirmed_email'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
     t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
+  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key 'active_storage_variant_records', 'active_storage_blobs', column: 'blob_id'
   add_foreign_key 'assignments', 'tasks'
   add_foreign_key 'assignments', 'users'
   add_foreign_key 'tasks', 'users'
