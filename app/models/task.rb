47ae760b0e84548d6c1dd05dc43572ab6a7112e5
tobiasz-p <tpa0528@gmail.com>
2022-03-21 17:43:41 +0100

    Add destroying assignments on task destroy

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,7 @@
 class Task < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: :user_id
 
-  has_many :assignments
+  has_many :assignments, dependent: :destroy
   has_many :assignees, through: :assignments
 
   validates :title, presence: true
